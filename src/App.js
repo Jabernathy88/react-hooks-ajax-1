a377c5e52dc4c87ce631aea1049c73fdaf35eac4
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-01 10:24:28 -0400

    Test with my other API.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        // `http://hn.algolia.com/api/v1/search?query=${query}`,
+        `https://kusoft-pen-pal.herokuapp.com/api/v1/users/1`
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h2>Hello from Fragment App.js</h2>
+      <input
+        className="px-2"
+        type="text"
+        value={query}
+        onChage={event => setQuery(event.target.value)}
+      />
+      <button type="button" onClick={() => setSearch(query)}>
+        Search
+      </button>
+      <div className="pt-2">
+        {data.hits.map(item => (
+          <div key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </div>
+        ))}
+      </div>
+    </Fragment>
+
+
   );
 }
 
